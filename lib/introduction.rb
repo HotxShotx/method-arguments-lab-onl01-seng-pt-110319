5358f5222128d838c3a7d1d1f5796eaf8c1e0f58
outstanding-reference-3248 <omar.benjamin377@gmail.com>
2019-11-11 23:29:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,9 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
- puts introduction(Liz)
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+
